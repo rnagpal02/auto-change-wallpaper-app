40333e71d1205c1b1c48ff99ad5736d7a92b2943
Rajin N <rnagpal02@outlook.com>
2023-01-03 13:03:59 -0600

    Added text indication to select wallpaper

--- app/src/main/java/com/example/autochangewallpaperapp/MainActivity.java
+++ app/src/main/java/com/example/autochangewallpaperapp/MainActivity.java
@@ -10,6 +10,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,28 +20,49 @@ public class MainActivity extends AppCompatActivity {
     private final String EVENING_WALLPAPER = "evening_wallpaper";
     private final String NIGHT_WALLPAPER = "night_wallpaper";
 
+    private TextView morningWallpaperText;
+    private TextView afternoonWallpaperText;
+    private TextView eveningWallpaperText;
+    private TextView nightWallpaperText;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        findViewById(R.id.morningWallpaperButton).setOnClickListener(wallpaperButtonListener);
-        findViewById(R.id.afternoonWallpaperButton).setOnClickListener(wallpaperButtonListener);
-        findViewById(R.id.eveningWallpaperButton).setOnClickListener(wallpaperButtonListener);
-        findViewById(R.id.nightWallpaperButton).setOnClickListener(wallpaperButtonListener);
+        findViewById(R.id.chooseMorningWallpaper).setOnClickListener(wallpaperButtonListener);
+        findViewById(R.id.chooseAfternoonWallpaper).setOnClickListener(wallpaperButtonListener);
+        findViewById(R.id.chooseEveningWallpaper).setOnClickListener(wallpaperButtonListener);
+        findViewById(R.id.chooseNightWallpaper).setOnClickListener(wallpaperButtonListener);
+
+        morningWallpaperText = findViewById(R.id.morningWallpaperText);
+        afternoonWallpaperText = findViewById(R.id.afternoonWallpaperText);
+        eveningWallpaperText = findViewById(R.id.eveningWallpaperText);
+        nightWallpaperText = findViewById(R.id.nightWallpaperText);
+
+        updateUI();
+    }
+
+    private void updateUI() {
+        SharedPreferences preferences = getPreferences(MODE_PRIVATE);
+        morningWallpaperText.setVisibility(preferences.contains(MORNING_WALLPAPER) ? View.INVISIBLE : View.VISIBLE);
+        afternoonWallpaperText.setVisibility(preferences.contains(AFTERNOON_WALLPAPER) ? View.INVISIBLE : View.VISIBLE);
+        eveningWallpaperText.setVisibility(preferences.contains(EVENING_WALLPAPER) ? View.INVISIBLE : View.VISIBLE);
+        nightWallpaperText.setVisibility(preferences.contains(NIGHT_WALLPAPER) ? View.INVISIBLE : View.VISIBLE);
     }
 
     private final View.OnClickListener wallpaperButtonListener = new View.OnClickListener() {
         final String TAG = "WALLPAPER_BUTTON_LISTENER";
         public void onClick(View v) {
             String targetWallpaper;
-            if(v == findViewById(R.id.morningWallpaperButton)) {
+            if(v == findViewById(R.id.chooseMorningWallpaper)) {
                 targetWallpaper = MORNING_WALLPAPER;
-            } else if(v == findViewById(R.id.afternoonWallpaperButton)) {
+            } else if(v == findViewById(R.id.chooseAfternoonWallpaper)) {
                 targetWallpaper = AFTERNOON_WALLPAPER;
-            } else if(v == findViewById(R.id.eveningWallpaperButton)) {
+            } else if(v == findViewById(R.id.chooseEveningWallpaper)) {
                 targetWallpaper = EVENING_WALLPAPER;
-            } else if(v == findViewById(R.id.nightWallpaperButton)) {
+            } else if(v == findViewById(R.id.chooseNightWallpaper)) {
                 targetWallpaper = NIGHT_WALLPAPER;
             } else {
                 Log.e(TAG, "Unhandled button click");
@@ -68,7 +90,9 @@ public class MainActivity extends AppCompatActivity {
                         String targetWallpaper = preferences.getString(TARGET_WALLPAPER, default_str);
                         if (!targetWallpaper.equals(default_str)) {
                             SharedPreferences.Editor editor = preferences.edit();
-                            editor.putString(TARGET_WALLPAPER, result.toString());
+                            editor.putString(targetWallpaper, result.toString());
+                            editor.apply();
+                            updateUI();
                         } else {
                             Log.e(TAG, "Target wallpaper not found");
                         }
